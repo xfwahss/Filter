dadaaf8b789ebd9328c8976806d7d7dd6ba7d617
xfwahss <xfwahss@qq.com>
2024-04-01 17:54:05 +0800

    Add build

---
+++ build/CMakeFiles/kalman.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/work/kalman-filter/src/io/src/FileIO.cc" "CMakeFiles/kalman.dir/io/src/FileIO.cc.o" "gcc" "CMakeFiles/kalman.dir/io/src/FileIO.cc.o.d"
+  "/home/work/kalman-filter/src/io/src/InitState.cc" "CMakeFiles/kalman.dir/io/src/InitState.cc.o" "gcc" "CMakeFiles/kalman.dir/io/src/InitState.cc.o.d"
+  "/home/work/kalman-filter/src/io/src/MatFileIO.cc" "CMakeFiles/kalman.dir/io/src/MatFileIO.cc.o" "gcc" "CMakeFiles/kalman.dir/io/src/MatFileIO.cc.o.d"
+  "/home/work/kalman-filter/src/kalman/FilterIO.cpp" "CMakeFiles/kalman.dir/kalman/FilterIO.cpp.o" "gcc" "CMakeFiles/kalman.dir/kalman/FilterIO.cpp.o.d"
+  "/home/work/kalman-filter/src/kalman/KalmanFilter.cpp" "CMakeFiles/kalman.dir/kalman/KalmanFilter.cpp.o" "gcc" "CMakeFiles/kalman.dir/kalman/KalmanFilter.cpp.o.d"
+  "/home/work/kalman-filter/src/kalman/test.cpp" "CMakeFiles/kalman.dir/kalman/test.cpp.o" "gcc" "CMakeFiles/kalman.dir/kalman/test.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/kalman.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kalman.dir/io/src/FileIO.cc.o"
+  "CMakeFiles/kalman.dir/io/src/FileIO.cc.o.d"
+  "CMakeFiles/kalman.dir/io/src/InitState.cc.o"
+  "CMakeFiles/kalman.dir/io/src/InitState.cc.o.d"
+  "CMakeFiles/kalman.dir/io/src/MatFileIO.cc.o"
+  "CMakeFiles/kalman.dir/io/src/MatFileIO.cc.o.d"
+  "CMakeFiles/kalman.dir/kalman/FilterIO.cpp.o"
+  "CMakeFiles/kalman.dir/kalman/FilterIO.cpp.o.d"
+  "CMakeFiles/kalman.dir/kalman/KalmanFilter.cpp.o"
+  "CMakeFiles/kalman.dir/kalman/KalmanFilter.cpp.o.d"
+  "CMakeFiles/kalman.dir/kalman/test.cpp.o"
+  "CMakeFiles/kalman.dir/kalman/test.cpp.o.d"
+  "lib/libkalman.pdb"
+  "lib/libkalman.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kalman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
