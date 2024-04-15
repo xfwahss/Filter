292ea5df4b428dadf4139eb7eeee60b839f859e3
Happy-Grass <3564494961@qq.com>
2024-04-15 11:31:14 +0000

    反硝化系统

---
+++ src/reactor/include/Denitrification.h
@@ -0,0 +1,59 @@
+#ifndef DENITRIFICATION_H
+#define DENITRIFICATION_H
+#define _USE_MATH_DEFINES 
+#include<cmath>
+
+struct denitrification_status {
+    double rn0;     // 零阶反硝化速率基值
+    double knb1;    // 一阶反硝化速率常数
+    double Tnc;     // 临界温度
+    double theta_n; // 温度系数
+    double c_noxc;  // 临界溶解氧浓度
+    double c_noxo;  // 最优溶解氧浓度
+    denitrification_status() = default;
+    denitrification_status(const double &rn0, const double &knb1,
+                           const double &Tnc, const double &theta_n,
+                           const double &c_noxc, const double &c_noxo)
+        : rn0(rn0), knb1(knb1), Tnc(Tnc), theta_n(theta_n), c_noxc(c_noxc),
+          c_noxo(c_noxo){};
+};
+
+class Denitrification {
+  private:
+    denitrification_status coeffecients;
+    double beta; // 曲率系数，溶解氧限制函数曲率
+    double fnox(const double &c_ox);
+    double kn1(const double &T);
+
+  public:
+    Denitrification();
+    ~Denitrification();
+
+    /***************************************************************
+     *  @brief    初始化反硝化过程状态
+     *  @param    rn0, 0阶反应速率
+     *  @param    knb1, 20度时的反硝化速率常数
+     *  @param    Tnc, 临界温度
+     *  @param    theta_n, 温度系数
+     *  @param    c_noxc, 临界溶解氧浓度
+     *  @param    c_noxo, 最佳溶解氧浓度
+     *  @param    beta, 曲率系数，溶解氧限制曲线 beta=1时为直线
+     *  @Sample usage:     函数的使用方法
+     **************************************************************/
+    void init(const double &rn0, const double &knb1, const double &Tnc,
+              const double &theta_n, const double &c_noxc, const double &c_noxo,
+              const double &beta=1.0);
+    /***************************************************************
+     *  @brief    依据溶解氧浓度，硝态氮浓度，温度计算反硝化速率
+     *  @param    c_ox, 溶解氧浓度
+     *  @param    T, 温度
+     *  @param    c_ni, 硝态氮浓度
+     *  @Sample usage:     函数的使用方法
+     **************************************************************/
+    double rate(const double &c_ox, const double &T, const double &c_ni);
+    // 获取当前系统中的反硝化状态
+    denitrification_status current_status();
+    // 更新当前系统中的反硝化状态
+    void update_status(const denitrification_status &updated_status);
+};
+#endif
---
+++ src/reactor/src/Denitrification.cpp
@@ -0,0 +1,50 @@
+#include "../include/Denitrification.h"
+
+Denitrification::Denitrification() {}
+Denitrification::~Denitrification() {}
+
+void Denitrification::init(const double &rn0, const double &knb1,
+                           const double &Tnc, const double &theta_n,
+                           const double &c_noxc, const double &c_noxo,
+                           const double &beta) {
+    denitrification_status value(rn0, knb1, Tnc, theta_n, c_noxc, c_noxo);
+    coeffecients = value;
+    this->beta   = beta;
+}
+
+denitrification_status Denitrification::current_status() {
+    return coeffecients;
+}
+
+void Denitrification::update_status(
+    const denitrification_status &updated_status) {
+    coeffecients = updated_status;
+}
+
+double Denitrification::fnox(const double &c_ox) {
+    if (c_ox <= coeffecients.c_noxo) {
+        return 1.0;
+    } else if (c_ox < coeffecients.c_noxc) {
+        double numerator = coeffecients.c_noxc - c_ox;
+        double denominator =
+            (coeffecients.c_noxc - coeffecients.c_noxo) +
+            (std::pow(M_E, beta) - M_E) * (c_ox - coeffecients.c_noxo);
+        return numerator / denominator;
+    } else {
+        return 0.0;
+    }
+}
+
+double Denitrification::kn1(const double &T) {
+    if (T < coeffecients.Tnc) {
+        return 0;
+    } else {
+        return coeffecients.knb1 * std::pow(coeffecients.theta_n, T - 20);
+    }
+}
+
+double Denitrification::rate(const double &c_ox, const double &T, const double &c_ni){
+    double fnox = this->fnox(c_ox);
+    double kn1 = this->kn1(T);
+    return coeffecients.rn0 + fnox * kn1 * c_ni;
+}
