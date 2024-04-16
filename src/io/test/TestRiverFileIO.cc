0d2cfa7f91f571f8799d330b8c09a0ead32b8f21
Happy-Grass <3564494961@qq.com>
2024-04-16 06:33:11 +0000

    河流数据读取测试

---
+++ src/io/test/TestRiverFileIO.cc
@@ -0,0 +1,22 @@
+#include "../include/RiverFileIO.h"
+#include <iostream>
+
+/*
+编译命令：
+g++ -o TestRiverFileIO.o TestRiverFileIO.cc "../src/RiverFileIO.cc" "../src/FileIO.cc"
+*/
+
+int main() {
+    RiverFileIO river("./bai_river.txt");
+    bool status = true;
+    while (status) {
+        status = river.readline();
+        if (status) {
+            std::cout << "Flow:  " << river.data.flow
+            << "\tammonia:  " << river.data.ammonia 
+            << "\tnitrate:  " << river.data.nitrate 
+            << "\torganic:  " << river.data.organic 
+            << std::endl;
+        }
+    }
+}
