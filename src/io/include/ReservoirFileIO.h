5bbba4cf6de74dcea48da93b58dce7ebee1b279b
Happy-Grass <3564494961@qq.com>
2024-04-17 04:21:19 +0000

    更新水库存储结构体位置，防止头文件引用死锁

--- src/io/include/ReservoirFileIO.h
+++ src/io/include/ReservoirFileIO.h
@@ -2,7 +2,17 @@
 #define RESERVOIRFILEIO_H
 #include "FileIO.h"
 #include<fstream>
-#include"../../reactor/include/Reservoir.h"
+
+struct reservoir_status {
+    double wl;   // 水库水位
+    double c_no; // 水库有机氮浓度
+    double c_na; // 水库氨氮浓度
+    double c_nn; // 水库硝态氮浓度
+    reservoir_status() = default;
+    reservoir_status(const double &wl, const double &c_no, const double &c_na,
+                     const double &c_nn)
+        : wl(wl), c_no(c_no), c_na(c_na), c_nn(c_nn) {}
+};
 
 class ReservoirFileIO : public FileIO {
   private:
