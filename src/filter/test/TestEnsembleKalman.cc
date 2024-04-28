e0cf3b92586e81d1013f0a94fa4d979e6602ef42
Happy-Grass <3564494961@qq.com>
2024-04-28 01:50:26 +0000

    update test value

--- src/filter/test/TestEnsembleKalman.cc
+++ src/filter/test/TestEnsembleKalman.cc
@@ -18,15 +18,23 @@ class Test {
 int main() {
     Eigen::VectorXd mean(3);
     Eigen::MatrixXd cov(3, 3);
+
+    Eigen::VectorXd Q(3);
+    Q << 1, 1, 1;
     mean << 1, 2, 3;
-    cov << 4, 0, 0, 
-           0, 1, 0,
-           0, 0, 1;
+    cov << 4, 0.4, 0.8, 
+           0.4, 1, 0.2,
+           0.8, 0.2, 1;
 
     EnsembleKalmanFilter<Test> enkal(10000, mean, cov);
+    enkal.set_process_error_variance(Q);
 
     std::cout << enkal.current_status() << std::endl;
     std::cout << enkal.current_covariance() << std::endl;
+    enkal.predict(1);
+    std::cout << enkal.current_status() << std::endl;
+    std::cout << enkal.current_covariance() << std::endl;
+
     enkal.predict(1);
     std::cout << enkal.current_status() << std::endl;
     std::cout << enkal.current_covariance() << std::endl;
