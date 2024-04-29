48e84636236ca82b8828297f436f648ca2997e6c
Happy-Grass <3564494961@qq.com>
2024-04-29 02:49:05 +0000

    Add test for Nitrification

--- src/reactor/test/testNitrification.cc
+++ src/reactor/test/testNitrification.cc
@@ -6,8 +6,14 @@ int main(){
     nitriac.init(0.1, 0.01, 0.5, 2, 8, 0.8, 5, 0);
     double rate = nitriac.rate(7, 20, 5);
     std::cout << rate << std::endl;
-    nitrify_status update_values(0.2, 0.01, 0.5, 2, 8, 0.9, 10);
-    nitriac.update_status(update_values);
+    nitri_status update_values(0.2, 0.01, 0.5, 2, 8, 0.9, 10);
+    nitriac.update(update_values);
     rate = nitriac.rate(7, 20, 5);
     std::cout << rate << std::endl;
+
+    Eigen::VectorXd v = static_cast<Eigen::VectorXd>(nitriac.get_status());
+    std::cout << v << std::endl;
+    nitri_status nnn;
+    nnn = v;
+    std::cout << nitriac.rate(7, 20, 5)<<std::endl;
 }
