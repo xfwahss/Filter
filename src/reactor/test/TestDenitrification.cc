f460b926d0b5911869974f7f796e5ae34a8c00b0
Happy-Grass <3564494961@qq.com>
2024-04-29 02:02:17 +0000

    test denitrification

--- src/reactor/test/TestDenitrification.cc
+++ src/reactor/test/TestDenitrification.cc
@@ -6,10 +6,15 @@ int main(){
     denitritor.init(0.1, 0.01, 5, 1.5, 10, 7, 1.0);
     double rate = denitritor.rate(7, 10, 2.5);
     std::cout<< rate << std::endl;
-    denitrification_status up_status(0.1, 0.02, 10, 1.5, 12, 8);
-    denitritor.update_status(up_status);
+    deni_status up_status(0.1, 0.02, 10, 1.5, 12, 8);
+    denitritor.update(up_status);
     rate = denitritor.rate(7, 10, 2.5);
     std::cout<< rate << std::endl;
-
-
+    Eigen::VectorXd a = static_cast<Eigen::VectorXd>(denitritor.get_status());
+    std::cout << a << std::endl;
+    Eigen::VectorXd b(6);
+    b << 1, 2, 3, 4, 5, 6;
+    deni_status newdeni;
+    newdeni = b;
+    std::cout << newdeni.rn0 << std::endl;
 }
