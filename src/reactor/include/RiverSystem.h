7fbecfa7f237f058f8fb735250e080234531a91b
Happy-Grass <3564494961@qq.com>
2024-05-05 05:23:02 +0000

    use map to store rivers and overload operator<<

--- src/reactor/include/RiverSystem.h
+++ src/reactor/include/RiverSystem.h
@@ -1,19 +1,23 @@
 #ifndef RIVER_SYSTEM_H
 #define RIVER_SYSTEM_H
 #include "River.h"
-#include <vector>
+#include <iostream>
+#include <map>
 
 class RiverSystem {
   private:
-    std::vector<River *> rivers;
+    std::map<std::string, River *> rivers;
     Eigen::VectorXd status;
+    Eigen::VectorXd sum();
 
   public:
     RiverSystem();
     ~RiverSystem();
-    void add_river(River &river);
+    void add_river(std::string river_name, River &river);
     Eigen::VectorXd get_status();
     void update(Eigen::VectorXd status);
-    void predict(const double& dt);
+    void predict(const double &dt);
+    friend std::ostream &operator<<(std::ostream &os,
+                                    const RiverSystem &rivers);
 };
 #endif
--- src/reactor/src/RiverSystem.cc
+++ src/reactor/src/RiverSystem.cc
@@ -3,10 +3,18 @@
 RiverSystem::RiverSystem() {}
 RiverSystem::~RiverSystem() {}
 
-void RiverSystem::add_river(River &river){
-    rivers.push_back(&river);
+void RiverSystem::add_river(std::string river_name, River &river) {
+    rivers[river_name] = &river;
 }
 
-Eigen::VectorXd RiverSystem::get_status(){};
+// Eigen::VectorXd RiverSystem::get_status(){};
 void RiverSystem::update(Eigen::VectorXd status){};
-void predict(const double& dt);
+void predict(const double &dt);
+std::ostream &operator<<(std::ostream &os, const RiverSystem &river) {
+    std::map<std::string, River *>::const_iterator iter = river.rivers.begin();
+    while (iter != river.rivers.end()) {
+        os << iter->first << std::endl;
+        ++iter;
+    }
+    return os;
+}
