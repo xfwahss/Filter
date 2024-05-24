033cdc7deeac9cf6e7614ea0d5b55d2c995166b4
xfwahss <xfwahss@qq.com>
2024-05-24 18:42:47 +0800

    update predict method

--- src/model/src/Validate.cc
+++ src/model/src/Validate.cc
@@ -2,12 +2,11 @@
 #include "../../io/include/FilterIO.h"
 
 // 负责粒子更新步, 将状态向量分配给机理模型
-class Model: public EnsembleModel {
+class Model : public EnsembleModel {
   public:
-    Eigen::VectorXd predict(const double &dt) {
-      Eigen::VectorXd s = get_status();
-      s = constant_predict(s, dt);
-      return s;
+    Eigen::VectorXd predict(const double &dt, const Eigen::VectorXd &status) {
+        Eigen::VectorXd s = constant_predict(status, dt);
+        return s;
     }
 };
 
@@ -15,7 +14,7 @@ int main() {
 
     EnsembleKalmanFilter<Model> enkal(2000);
     FilterIO modelio("../data/TestModelIO_in.xlsx",
-                    "../data/TestModelIO_out.xlsx");
+                     "../data/TestModelIO_out.xlsx");
     enkal.init(modelio.get_init_X(), modelio.get_init_P(), modelio.get_H(),
                modelio.get_Q());
     enkal.batch_assimilation(&modelio, 1);
--- src/model/src/velocity_tracker.cc
+++ src/model/src/velocity_tracker.cc
@@ -3,9 +3,8 @@
 // 负责粒子更新步, 将状态向量分配给机理模型
 class Model: public EnsembleModel {
   public:
-    Eigen::VectorXd predict(const double &dt) {
-      Eigen::VectorXd s = get_status();
-      s = const_1derivate_predict(s, dt);
+    Eigen::VectorXd predict(const double &dt, const Eigen::VectorXd &status) {
+      Eigen::VectorXd s = const_1derivate_predict(status, dt);
       return s;
     }
 };
