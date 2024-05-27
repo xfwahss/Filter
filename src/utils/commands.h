89c9ff0baf34fac00860d684ca7745377b19f66e
xfwahss <xfwahss@qq.com>
2024-05-27 09:17:18 +0800

    add method for multivatiate_gaussian_random

--- src/utils/commands.h
+++ src/utils/commands.h
@@ -1,7 +1,11 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
+#include <Eigen/Dense>
+#include <initializer_list>
+#include <random>
 #include <string>
 #include <unordered_map>
+
 
 namespace tools {
 std::unordered_map<std::string, std::string> get_args(int argc, char *argv[]) {
@@ -19,11 +23,46 @@ std::unordered_map<std::string, std::string> get_args(int argc, char *argv[]) {
     return options;
 }
 
-double handle_miss(const double &value){
-    if(value == -999.0){
+double handle_miss(const double &value) {
+    if (value == -999.0) {
         return 0;
     } else {
         return value;
+    }
+}
+
+Eigen::VectorXd multivariate_gaussian_random(Eigen::VectorXd &status,
+                                             Eigen::MatrixXd &covariance) {
+    std::random_device seed;
+    std::mt19937_64 engine(seed());
+    std::normal_distribution<double> dist(0.0, 1.0);
+
+    // 使用Eigen库计算Cholesky分解
+    Eigen::LLT<Eigen::MatrixXd> lltOfCov(covariance);
+    Eigen::MatrixXd L = lltOfCov.matrixL();
+
+    // 生成随机数
+    Eigen::VectorXd z(status.size());
+    for (int i = 0; i < status.size(); ++i) {
+        z(i) = dist(engine);
+    }
+    Eigen::VectorXd random_number = status + L * z;
+    return random_number;
+}
+
+double exclude_mean(std::initializer_list<double> args) {
+    double sum   = 0;
+    double count = 0;
+    for (double arg : args) {
+        if (arg != -999) {
+            sum += arg;
+            ++count;
+        };
+    }
+    if (count == 0) {
+        return 0;
+    } else {
+        return sum / count;
     }
 }
 } // namespace tools
