d915001aeb990f57f04639d663936b45fce3cd07
xfwahss <xfwahss@qq.com>
2024-06-04 10:04:20 +0800

    scripts to visualize synthetic data

---
+++ src/model/scripts/synthetic_data.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from plotstyles.fonts import global_fonts
+from plotstyles import tools
+from matplotlib import pyplot as plt
+import os
+import subprocess
+import numpy as np
+
+def run_model(base_dir, bin_dir, toggle=True):
+    exe_path = os.path.join(base_dir, bin_dir, "ModelReservoir.exe")
+    if toggle:
+        subprocess.run([exe_path])
+    return
+
+def visualize(ax, fin, fout, var_name):
+    fin_data = pd.read_excel(fin, "Res_Force")
+    fout_data = pd.read_excel(fout, "Value")
+    x = list(range(fout_data[var_name].size))
+    ax.plot(x, fout_data[var_name])
+
+if __name__=="__main__":
+    base_dir = "D:/Gitlocal/Filter"
+    os.chdir(os.path.join(base_dir, "build"))
+    run_model(base_dir, "build/bin", False)
+
+    fig = plt.figure(figsize=tools.cm2inch([12, 8]))
+    ax = fig.add_subplot(111)
+    fin = os.path.join(base_dir, "test", "data", "Synthetic_Modeling.xlsx")
+    fout = os.path.join(base_dir, "test", "output", "Synthetic_Modeling_out.xlsx")
+    # visualize(ax, fin, fout, "c_no")
+    visualize(ax, fin, fout, "c_na")
+    # visualize(ax, fin, fout, "c_nn")
+
+    plt.show()
