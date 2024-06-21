0b80ed4cdf8f76957585ea5a888b0646006b9ea7
xfwahss <xfwahss@qq.com>
2024-06-21 08:18:53 +0800

    update comment

--- src/filter/include/EnsembleKalmanFilter.h
+++ src/filter/include/EnsembleKalmanFilter.h
@@ -1,64 +1,176 @@
+/*!
+ * @file EnsembleKalmanFilter.h
+ * @brief 集合卡尔曼滤波模板
+ * 1. 采用高斯分布采样
+ * 2. 模板类需要实现update(VectorXd)方法和predict()方法以及获取当前状态的方法，
+ *    预测过程使用模型的精确预测，不确定度由集合卡尔曼滤波给定
+ * 3. 每次分析步过后都会重新采样, 确保采样后的集合均值和协方差与给定的一致
+ * @author xfwahss
+ * @version main-v0.2
+ * @date 2024-06-04
+ */
 #ifndef ENSEMBLE_KALMAN_FILTER_H
 #define ENSEMBLE_KALMAN_FILTER_H
 #include "../../io/include/FilterIO.h"
-#include "FilterBase.h"
 #include "../../utils/include/umath.h"
+#include "FilterBase.h"
 #include <Eigen/Dense>
 #include <random>
 #include <vector>
-
-
-// 集合卡尔曼滤波模板类，只能实现高斯分布采样
-// 模拟类需要实现 update(接收滤波器更新了的参数)， predict(向前推进)方法,
-// 和获取当前状态的方法 预测设定为精确预测吧，不确定度由集合卡尔曼滤波类实现
-// 应该是每融合一次都需要重新采样, 这样才能确保集合的分布是融合过后的
 
 template <class T> class EnsembleKalmanFilter : public FilterBase {
   public:
     EnsembleKalmanFilter(const int &ensemble_size);
     ~EnsembleKalmanFilter();
+
     void init(Eigen::VectorXd X, Eigen::MatrixXd P, Eigen::MatrixXd H,
               Eigen::VectorXd Q);
 
+    // 单步同化
     void step_assimilation(const double &dt, const Eigen::VectorXd &z,
-                           const Eigen::MatrixXd &R);
-    void batch_assimilation(FilterIO *modelio, const double &dt,
-                            const Eigen::MatrixXd &R = Eigen::MatrixXd());
-    Eigen::VectorXd get_status();
-    Eigen::MatrixXd get_covariance();
+                           const Eigen::MatrixXd &R, bool ensure_pos);
+    // 批量同化
+    /*
+     * @param ensure_pos 是否保证采样的值全为正数
+     */
+    void batch_assimilation(FilterIO *filterio, const double &dt,
+                            const Eigen::MatrixXd &R = Eigen::MatrixXd(),
+                            bool ensure_pos          = false);
+    Eigen::VectorXd status();
+    Eigen::MatrixXd covariance();
 
   private:
+    // 存储指向集合中子模型的指针
     std::vector<T *> ensemble;
-    // 添加过程预测的误差，超参数，人为给定
     // 采用单个推进，误差为向量形式,给定每个方差的大小，均值为0，使用正态随机数
     Eigen::VectorXd Q;
     // 观测矩阵H
     Eigen::MatrixXd H;
-
     // 根据给定的方差生成一个状态的预测随机误差
     Eigen::VectorXd generate_process_error();
 
-    void ensemble_construct(const int &ensemble_size);
-    void sample();
-    void sample_predict(const double &dt);
-    void ensemble_destruct();
+    // 构建集合
+    void construct(const int &ensemble_size);
+    // 销毁集合
+    void destruct();
 
-    void predict(const double &dt);
+    // 状态采样
+    void sample(bool ensure_pos);
+    // 样本状态预测
+    void sample_predict(const double &dt);
+
+    // 利用样本预测估计整体
+    void predict(const double &dt, bool ensure_pos);
     void update(Eigen::VectorXd z, Eigen::MatrixXd R);
 };
 
 template <class T>
 EnsembleKalmanFilter<T>::EnsembleKalmanFilter(const int &ensemble_size) {
     ensemble.reserve(ensemble_size);
-    ensemble_construct(ensemble_size);
+    construct(ensemble_size);
 }
 
 template <class T>
-void EnsembleKalmanFilter<T>::ensemble_construct(const int &ensemble_size) {
+void EnsembleKalmanFilter<T>::step_assimilation(const double &dt,
+                                                const Eigen::VectorXd &z,
+                                                const Eigen::MatrixXd &R,
+                                                bool ensure_pos) {
+    predict(dt, ensure_pos);
+    update(z, R);
+}
+
+template <class T>
+void EnsembleKalmanFilter<T>::batch_assimilation(FilterIO *filterio,
+                                                 const double &dt,
+                                                 const Eigen::MatrixXd &R,
+                                                 bool ensure_pos) {
+    Eigen::VectorXd z;
+    Eigen::MatrixXd r_file;
+    filterio->write_headers();
+    if (R.size() == 0) {
+        filterio->read(z, r_file);
+        while (z.size() != 0) {
+            this->step_assimilation(dt, z, r_file, ensure_pos);
+            filterio->write_x(X, "X", 1, 2);
+            filterio->write_P(P, "P", 1, 2);
+            filterio->read(z, r_file);
+        }
+    } else {
+        filterio->read(z, r_file);
+        while (z.size() != 0) {
+            this->step_assimilation(dt, z, R, ensure_pos);
+            filterio->write_x(X, "X", 1, 2);
+            filterio->write_P(P, "P", 1, 2);
+            filterio->read(z, r_file);
+        }
+    }
+}
+
+template <class T>
+void EnsembleKalmanFilter<T>::construct(const int &ensemble_size) {
     for (int i = 0; i < ensemble_size; i++) {
         T *ptr = new T();
         ensemble.push_back(ptr);
     }
+}
+
+template <class T> void EnsembleKalmanFilter<T>::destruct() {
+    for (int i = 0; i < ensemble.size(); i++) {
+        delete ensemble[i];
+    }
+    ensemble.clear();
+}
+
+template <class T> void EnsembleKalmanFilter<T>::sample(bool ensure_pos) {
+    for (int i = 0; i < ensemble.size(); ++i) {
+        Eigen::VectorXd random_num;
+        if (ensure_pos) {
+            random_num = umath::pos_multi_gauss_random(X, P);
+        } else {
+            random_num = umath::multivariate_gaussian_random(X, P);
+        }
+        ensemble[i]->update(random_num);
+    }
+}
+
+template <class T>
+void EnsembleKalmanFilter<T>::sample_predict(const double &dt) {
+    for (int i = 0; i < ensemble.size(); ++i) {
+        ensemble[i]->step(dt);
+    }
+}
+
+template <class T>
+void EnsembleKalmanFilter<T>::predict(const double &dt, bool ensure_pos) {
+    sample(ensure_pos);
+    sample_predict(dt);
+
+    // 计算均值向量
+    Eigen::VectorXd mean = Eigen::VectorXd::Zero(X.rows());
+    for (int i = 0; i < ensemble.size(); ++i) {
+        mean += ensemble[i]->get_status();
+    }
+    mean                /= ensemble.size();
+
+    // 计算协方差矩阵
+    Eigen::MatrixXd cov  = Eigen::MatrixXd::Zero(P.rows(), P.cols());
+    for (int j = 0; j < ensemble.size(); ++j) {
+        // 需要考虑过程预测引起的误差,添加了get_process_error
+        Eigen::VectorXd diff =
+            ensemble[j]->get_status() + generate_process_error() - mean;
+        cov += diff * diff.transpose();
+    }
+    cov /= ensemble.size() - 1;
+    X    = mean;
+    P    = cov;
+}
+
+template <class T>
+void EnsembleKalmanFilter<T>::update(Eigen::VectorXd z, Eigen::MatrixXd R) {
+    Eigen::MatrixXd K;
+    K = P * H.transpose() * (H * P * H.transpose() + R).inverse();
+    X = X + K * (z - H * X);
+    P = (Eigen::MatrixXd::Identity(P.rows(), P.cols()) - K * H) * P;
 }
 
 template <class T>
@@ -71,71 +183,7 @@ void EnsembleKalmanFilter<T>::init(Eigen::VectorXd X, Eigen::MatrixXd P,
 }
 
 template <class T> EnsembleKalmanFilter<T>::~EnsembleKalmanFilter() {
-    ensemble_destruct();
-}
-
-template <class T> void EnsembleKalmanFilter<T>::predict(const double &dt) {
-    sample();
-    sample_predict(dt);
-
-    // 计算均值向量
-    Eigen::VectorXd mean = Eigen::VectorXd::Zero(FilterBase::X.rows());
-    for (int i = 0; i < ensemble.size(); ++i) {
-        mean += ensemble[i]->get_status();
-    }
-    mean /= ensemble.size();
-
-    // 计算协方差矩阵
-    Eigen::MatrixXd cov =
-        Eigen::MatrixXd::Zero(FilterBase::P.rows(), FilterBase::P.cols());
-    for (int j = 0; j < ensemble.size(); ++j) {
-        // 需要考虑过程预测引起的误差,添加了get_process_error
-        Eigen::VectorXd diff =
-            ensemble[j]->get_status() + generate_process_error() - mean;
-        cov += diff * diff.transpose();
-    }
-    cov /= ensemble.size() - 1;
-    FilterBase::X = mean;
-    FilterBase::P = cov;
-}
-
-template <class T>
-void EnsembleKalmanFilter<T>::update(Eigen::VectorXd z, Eigen::MatrixXd R) {
-    Eigen::MatrixXd K =
-        P * H.transpose() * (H * P * H.transpose() + R).inverse();
-    X = X + K * (z - H * X);
-    P = (Eigen::MatrixXd::Identity(P.rows(), P.cols()) - K * H) * P;
-}
-
-template <class T>
-void EnsembleKalmanFilter<T>::step_assimilation(const double &dt,
-                                                const Eigen::VectorXd &z,
-                                                const Eigen::MatrixXd &R) {
-    predict(dt);
-    update(z, R);
-}
-
-template <class T> void EnsembleKalmanFilter<T>::sample() {
-    for (int i = 0; i < ensemble.size(); ++i) {
-        // Eigen::VectorXd random_num =
-        //     multivariate_gaussian_random(FilterBase::X, FilterBase::P);
-        Eigen::VectorXd random_num = umath::pos_multi_gauss_random(FilterBase::X, FilterBase::P);
-        ensemble[i]->update(random_num);
-    }
-}
-
-template <class T>
-void EnsembleKalmanFilter<T>::sample_predict(const double &dt) {
-    for (int i = 0; i < ensemble.size(); ++i) {
-        ensemble[i]->step(dt);
-    }
-}
-
-template <class T> void EnsembleKalmanFilter<T>::ensemble_destruct() {
-    for (int i = 0; i < ensemble.size(); i++) {
-        delete ensemble[i];
-    }
-    ensemble.clear();
+    destruct();
 }
 
 template <class T>
@@ -150,38 +198,12 @@ Eigen::VectorXd EnsembleKalmanFilter<T>::generate_process_error() {
     return error;
 }
 
-template <class T> Eigen::VectorXd EnsembleKalmanFilter<T>::get_status() {
+template <class T> Eigen::VectorXd EnsembleKalmanFilter<T>::status() {
     return FilterBase::X;
 }
 
-template <class T> Eigen::MatrixXd EnsembleKalmanFilter<T>::get_covariance() {
+template <class T> Eigen::MatrixXd EnsembleKalmanFilter<T>::covariance() {
     return FilterBase::P;
-}
-
-template <class T>
-void EnsembleKalmanFilter<T>::batch_assimilation(FilterIO *filterio,
-                                                 const double &dt,
-                                                 const Eigen::MatrixXd &R) {
-    Eigen::VectorXd z;
-    Eigen::MatrixXd r_file;
-    filterio->write_headers();
-    if (R.size() == 0) {
-        filterio->read(z, r_file);
-        while (z.size() != 0) {
-            this->step_assimilation(dt, z, r_file);
-            filterio->write_x(X, "X", 1, 2);
-            filterio->write_P(P, "P", 1, 2);
-            filterio->read(z, r_file);
-        }
-    } else {
-        filterio->read(z, r_file);
-        while (z.size() != 0) {
-            this->step_assimilation(dt, z, R);
-            filterio->write_x(X, "X", 1, 2);
-            filterio->write_P(P, "P", 1, 2);
-            filterio->read(z, r_file);
-        }
-    }
 }
 
 // 提供抽象类提供作为集合卡尔曼滤波的模板类基类
