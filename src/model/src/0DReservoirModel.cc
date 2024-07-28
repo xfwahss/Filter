1e90d00d3a0b28fe3133542f0e038b346155e841
xfwahss <xfwahss@qq.com>
2024-07-28 13:51:24 +0800

    update

--- src/model/src/0DReservoirModel.cc
+++ src/model/src/0DReservoirModel.cc
@@ -170,47 +170,90 @@ void simulate(
         Logger::log_vectorxd("status:{}", status, "0DReservoirModel");
     }
 }
-void write_process_params(
+std::string copyfile_and_write_params(
     const std::string &filename, const std::string &sheetname,
-    const std::optional<double> &ro0, const std::optional<double> &ko1,
-    const std::optional<double> &ra0, const std::optional<double> &kab1,
-    const std::optional<double> &foxmin, const std::optional<double> &c_oxc,
-    const std::optional<double> &c_oxo, const std::optional<double> &theta_a,
-    const std::optional<double> &T_c, const std::optional<double> &rn0,
-    const std::optional<double> &knb1, const std::optional<double> &Tnc,
-    const std::optional<double> &theta_n, const std::optional<double> &c_noxc,
-    const std::optional<double> &c_noxo) {
+    const std::optional<std::string> &id, const std::optional<double> &k_rpon,
+    const std::optional<double> &theta_rpon,
+    const std::optional<double> &k_lpon,
+    const std::optional<double> &theta_lpon, const std::optional<double> &k_don,
+    const std::optional<double> &theta_don, const std::optional<double> &rnit0,
+    const std::optional<double> &knit20, const std::optional<double> &foxmin,
+    const std::optional<double> &c_oxc_nit,
+    const std::optional<double> &c_oxo_nit,
+    const std::optional<double> &theta_nit,
+    const std::optional<double> &T_c_nit, const std::optional<double> &alpha,
+    const std::optional<double> &rdeni0, const std::optional<double> &kdeni20,
+    const std::optional<double> &Tc_deni,
+    const std::optional<double> &theta_deni,
+    const std::optional<double> &c_oxc_deni,
+    const std::optional<double> &c_oxo_deni, const std::optional<double> &beta,
+    const std::optional<double> &b_ndo_flux,
+    const std::optional<double> &b_amm_flux,
+    const std::optional<double> &b_nit_flux, const std::optional<double> &s_nrp,
+    const std::optional<double> &s_nlp, const std::optional<double> &alpha_rpon,
+    const std::optional<double> &alpha_lpon, const std::optional<double> &h0) {
     std::filesystem::path file_origin = filename;
-    FileIO::copyFile(file_origin, file_origin.parent_path().parent_path() /
-                                      "temp" / file_origin.filename());
-    ExcelIO input(filename, "wa");
-    int row_i[]         = {2, 3, 2, 3, 4, 5, 6, 7, 8, 2, 3, 4, 5, 6, 7};
-    int column_i[]      = {2, 2, 4, 4, 4, 4, 4, 4, 4, 6, 6, 6, 6, 6, 6};
-    std::string names[] = {
-        "Ammonification",  "Ammonification",  "Nitrification",
-        "Nitrification",   "Nitrification",   "Nitrification",
-        "Nitrification",   "Nitrification",   "Nitrification",
-        "Denitrification", "Denitrification", "Denitrification",
-        "Denitrification", "Denitrification", "Denitrification"};
-    int write_column_i[]        = {2, 3, 2, 3, 4, 5, 6, 7, 8, 2, 3, 4, 5, 6, 7};
-    std::optional<double> arr[] = {ro0,   ko1,   ra0,     kab1,   foxmin,
-                                   c_oxc, c_oxo, theta_a, T_c,    rn0,
-                                   knb1,  Tnc,   theta_n, c_noxc, c_noxo};
-    int i                       = 0;
+    std::filesystem::path copyed_filepath =
+        file_origin.parent_path().parent_path() / "temp" /
+        (*id + file_origin.filename().string());
+    FileIO::copyFile(file_origin, copyed_filepath);
+    ExcelIO input(copyed_filepath.string(), "wa");
+    int row_i[] = {2,  3,  4,  5,  6,  7,  10, 11, 12, 13, 14, 15, 16, 17, 20,
+                   21, 22, 23, 24, 25, 26, 29, 30, 31, 34, 35, 38, 39, 40};
+    std::string names[]         = {"Ammonification",
+                                   "Ammonification",
+                                   "Ammonification",
+                                   "Ammonification",
+                                   "Ammonification",
+                                   "Ammonification",
+                                   "Nitrification",
+                                   "Nitrification",
+                                   "Nitrification",
+                                   "Nitrification",
+                                   "Nitrification",
+                                   "Nitrification",
+                                   "Nitrification",
+                                   "Nitrification",
+                                   "Denitrification",
+                                   "Denitrification",
+                                   "Denitrification",
+                                   "Denitrification",
+                                   "Denitrification",
+                                   "Denitrification",
+                                   "Denitrification",
+                                   "Sediment",
+                                   "Sediment",
+                                   "Sediment",
+                                   "ParticleSettling",
+                                   "ParticleSettling",
+                                   "Other",
+                                   "Other",
+                                   "Other"};
+    int write_column_i[]        = {2, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6, 7, 8, 9, 2,
+                                   3, 4, 5, 6, 7, 8, 2, 3, 4, 2, 3, 2, 3, 4};
+    std::optional<double> arr[] = {
+        k_rpon,     theta_rpon, k_lpon,     theta_lpon, k_don,      theta_don,
+        rnit0,      knit20,     foxmin,     c_oxc_nit,  c_oxo_nit,  theta_nit,
+        T_c_nit,    alpha,      rdeni0,     kdeni20,    Tc_deni,    theta_deni,
+        c_oxc_deni, c_oxo_deni, beta,       b_ndo_flux, b_amm_flux, b_nit_flux,
+        s_nrp,      s_nlp,      alpha_rpon, alpha_lpon, h0};
+    int i = 0;
     for (std::optional<double> value : arr) {
         if (value.has_value()) {
-            input.write_cell(*value, sheetname, row_i[i], column_i[i]);
+            input.write_cell(*value, sheetname, row_i[i], 3);
             for (int j = 2; j < 2195; j++) {
                 input.write_cell(*value, names[i], j, write_column_i[i]);
             }
         }
         i++;
     }
+    return copyed_filepath.string();
 }
 
 template <size_t SIZE>
 void parse(int argc, char *argv[], std::string &filename_in,
-           std::string &filename_out, std::optional<double> (&v)[SIZE]) {
+           std::string &filename_out, std::optional<double> (&v)[SIZE],
+           std::string &id) {
     args::ArgumentParser parser("Model a 0D reservoir system",
                                 "End of options");
     args::HelpFlag help(parser, "help", "Help menu", {'h', "help"});
@@ -220,43 +263,83 @@ void parse(int argc, char *argv[], std::string &filename_in,
     args::ValueFlag<std::string> fileout(
         parser, "path", "the filepath of file output", {'o', "output"});
 
-    args::ValueFlag<double> ro0(
-        parser, "ro0", "0 order reaction rate of ammonification", {"ro0"});
-    args::ValueFlag<double> ko1(
-        parser, "ra0", "1 order reaction rate constant of ammonification",
-        {"ko1"});
+    args::ValueFlag<std::string> idin(
+        parser, "idin", "add prefix when copy the file input", {"id"});
+    args::ValueFlag<double> k_rpon(
+        parser, "k_rpon",
+        "1 order reaction rate of refactory particle organic nitrogen",
+        {"k_rpon"});
+    args::ValueFlag<double> theta_rpon(parser, "theta_rpon", "theta_rpon",
+                                       {"theta_rpon"});
 
-    args::ValueFlag<double> ra0(
-        parser, "ra0", "0 order reaction rate of nitrification", {"ra0"});
-    args::ValueFlag<double> kab1(
-        parser, "kab1", "1 order reaction rate constant of nitrification",
-        {"kab1"});
+    args::ValueFlag<double> k_lpon(
+        parser, "k_lpon",
+        "1 order reaction rate of labile particle organic nitrogen",
+        {"k_lpon"});
+    args::ValueFlag<double> theta_lpon(parser, "theta_lpon", "theta_lpon",
+                                       {"theta_lpon"});
+
+    args::ValueFlag<double> k_don(
+        parser, "k_don", "1 order reaction rate of dissolved organic nitrogen",
+        {"k_don"});
+    args::ValueFlag<double> theta_don(parser, "theta_don", "theta_don",
+                                      {"theta_don"});
+
+    args::ValueFlag<double> rnit0(
+        parser, "rnit0", "0 order reaction rate of nitrification", {"rnit0"});
+    args::ValueFlag<double> knit20(
+        parser, "knit20", "1 order reaction rate constant of nitrification",
+        {"knit20"});
     args::ValueFlag<double> foxmin(
         parser, "foxmin", "limit of oxygen of nitrification", {"foxmin"});
-    args::ValueFlag<double> c_oxc(
-        parser, "c_oxc", "critical oxygen of nitrification", {"c_oxc"});
-    args::ValueFlag<double> c_oxo(parser, "c_oxo",
-                                  "optimal oxygen of nitrification", {"c_oxo"});
-    args::ValueFlag<double> theta_a(parser, "theta_a",
-                                    "temperatural coefficient of nitrification",
-                                    {"theta_a"});
-    args::ValueFlag<double> T_c(
-        parser, "T_c", "critical temperature of nitrification", {"T_c"});
+    args::ValueFlag<double> c_oxc_nit(
+        parser, "c_oxc_nit", "critical oxygen of nitrification", {"c_oxc_nit"});
+    args::ValueFlag<double> c_oxo_nit(
+        parser, "c_oxo_nit", "optimal oxygen of nitrification", {"c_oxo_nit"});
+    args::ValueFlag<double> theta_nit(
+        parser, "theta_nit", "temperatural coefficient of nitrification",
+        {"theta_nit"});
+    args::ValueFlag<double> T_c_nit(parser, "T_c_nit",
+                                    "critical temperature of nitrification",
+                                    {"T_c_nit"});
+    args::ValueFlag<double> alpha(parser, "alpha", "curvlinear rate",
+                                  {"alpha"});
 
-    args::ValueFlag<double> rn0(
-        parser, "rn0", "0 order reaction rate of denitrification", {"rn0"});
-    args::ValueFlag<double> knb1(
-        parser, "knb1", "1 order reaction rate constant of denitrification",
-        {"knb1"});
-    args::ValueFlag<double> Tnc(
-        parser, "Tnc", "critical temperature of denitrification", {"Tnc"});
-    args::ValueFlag<double> theta_n(
-        parser, "theta_n", "temperatural coefficient of denitrification",
-        {"theta_n"});
-    args::ValueFlag<double> c_noxc(
-        parser, "c_noxc", "critical oxygen of denitrification", {"c_noxc"});
-    args::ValueFlag<double> c_noxo(
-        parser, "c_noxo", "optimal oxygen of denitrification", {"c_noxo"});
+    args::ValueFlag<double> rdeni0(parser, "rdeni0",
+                                   "0 order reaction rate of denitrification",
+                                   {"rdeni0"});
+    args::ValueFlag<double> kdeni20(
+        parser, "kdeni20", "1 order reaction rate constant of denitrification",
+        {"kdeni20"});
+    args::ValueFlag<double> Tc_deni(parser, "Tc_deni",
+                                    "critical temperature of denitrification",
+                                    {"Tc_deni"});
+    args::ValueFlag<double> theta_deni(
+        parser, "theta_deni", "temperatural coefficient of denitrification",
+        {"theta_deni"});
+    args::ValueFlag<double> c_oxc_deni(parser, "c_oxc_deni",
+                                       "critical oxygen of denitrification",
+                                       {"c_oxc_deni"});
+    args::ValueFlag<double> c_oxo_deni(parser, "c_oxo_deni",
+                                       "optimal oxygen of denitrification",
+                                       {"c_oxo_deni"});
+    args::ValueFlag<double> beta(parser, "beta", "curvature rate", {"beta"});
+
+    args::ValueFlag<double> b_ndo_flux(parser, "b_ndo_flux", "b_ndo_flux",
+                                       {"b_ndo_flux"});
+    args::ValueFlag<double> b_amm_flux(parser, "b_amm_flux", "b_amm_flux",
+                                       {"b_amm_flux"});
+    args::ValueFlag<double> b_nit_flux(parser, "b_nit_flux", "b_nit_flux",
+                                       {"b_nit_flux"});
+
+    args::ValueFlag<double> s_nrp(parser, "s_nrp", "s_nrp", {"s_nrp"});
+    args::ValueFlag<double> s_nlp(parser, "s_nlp", "s_nlp", {"s_nlp"});
+
+    args::ValueFlag<double> alpha_rpon(parser, "alpha_rpon", "alpha_rpon",
+                                       {"alpha_rpon"});
+    args::ValueFlag<double> alpha_lpon(parser, "alpha_lpon", "alpha_lpon",
+                                       {"alpha_lpon"});
+    args::ValueFlag<double> h0(parser, "h0", "h0", {"h0"});
     try {
         parser.ParseCLI(argc, argv);
     } catch (args::Help) {
@@ -274,51 +357,97 @@ void parse(int argc, char *argv[], std::string &filename_in,
     if (fileout) {
         filename_out = args::get(fileout);
     }
-    if (ro0) {
-        v[0] = args::get(ro0);
+    if (idin) {
+        id = args::get(idin);
     }
-    if (ko1) {
-        v[1] = args::get(ko1);
+    if (k_rpon) {
+        v[0] = args::get(k_rpon);
+    }
+    if (theta_rpon) {
+        v[1] = args::get(theta_rpon);
+    }
+    if (k_lpon) {
+        v[2] = args::get(k_lpon);
+    }
+    if (theta_lpon) {
+        v[3] = args::get(theta_lpon);
+    }
+    if (k_don) {
+        v[4] = args::get(k_don);
+    }
+    if (theta_don) {
+        v[5] = args::get(theta_don);
     }
 
-    if (ra0) {
-        v[2] = args::get(ra0);
+    if (rnit0) {
+        v[6] = args::get(rnit0);
     }
-    if (kab1) {
-        v[3] = args::get(kab1);
+    if (knit20) {
+        v[7] = args::get(knit20);
     }
     if (foxmin) {
-        v[4] = args::get(foxmin);
+        v[8] = args::get(foxmin);
     }
-    if (c_oxc) {
-        v[5] = args::get(c_oxc);
+    if (c_oxc_nit) {
+        v[9] = args::get(c_oxc_nit);
     }
-    if (c_oxo) {
-        v[6] = args::get(c_oxo);
+    if (c_oxo_nit) {
+        v[10] = args::get(c_oxo_nit);
     }
-    if (theta_a) {
-        v[7] = args::get(theta_a);
+    if (theta_nit) {
+        v[11] = args::get(theta_nit);
     }
-    if (T_c) {
-        v[8] = args::get(T_c);
+    if (T_c_nit) {
+        v[12] = args::get(T_c_nit);
     }
-    if (rn0) {
-        v[9] = args::get(rn0);
+    if (alpha) {
+        v[13] = args::get(alpha);
     }
-    if (knb1) {
-        v[10] = args::get(knb1);
+
+    if (rdeni0) {
+        v[14] = args::get(rdeni0);
     }
-    if (Tnc) {
-        v[11] = args::get(Tnc);
+    if (kdeni20) {
+        v[15] = args::get(kdeni20);
     }
-    if (theta_n) {
-        v[12] = args::get(theta_n);
+    if (Tc_deni) {
+        v[16] = args::get(Tc_deni);
     }
-    if (c_noxc) {
-        v[13] = args::get(c_noxc);
+    if (theta_deni) {
+        v[17] = args::get(theta_deni);
     }
-    if (c_noxo) {
-        v[14] = args::get(c_noxo);
+    if (c_oxc_deni) {
+        v[18] = args::get(c_oxc_deni);
+    }
+    if (c_oxo_deni) {
+        v[19] = args::get(c_oxo_deni);
+    }
+    if (beta) {
+        v[20] = args::get(beta);
+    }
+    if (b_ndo_flux) {
+        v[21] = args::get(b_ndo_flux);
+    }
+    if (b_amm_flux) {
+        v[22] = args::get(b_amm_flux);
+    }
+    if (b_nit_flux) {
+        v[23] = args::get(b_nit_flux);
+    }
+    if (s_nrp) {
+        v[24] = args::get(s_nrp);
+    }
+    if (s_nlp) {
+        v[25] = args::get(s_nlp);
+    }
+    if (alpha_rpon) {
+        v[26] = args::get(alpha_rpon);
+    }
+    if (alpha_lpon) {
+        v[27] = args::get(alpha_lpon);
+    }
+    if (h0) {
+        v[28] = args::get(h0);
     }
 }
 
@@ -337,21 +466,16 @@ double volume_to_wl(const double &volume) {
 }
 
 int main(int argc, char *argv[]) {
-    std::filesystem::path filename_in, filename_out;
-    filename_in                     = "test/data/0DReservoirModel_origin.xlsx";
-    filename_out                    = "test/data/0DReservoirModel_out.xlsx";
-    // std::optional<double> value[15];
-    // parse(argc, argv, filename_in, filename_out, value);
-
-    // write_process_params(filename_in, "process_lib", value[0], value[1],
-    //                      value[2], value[3], value[4], value[5], value[6],
-    //                      value[7], value[8], value[9], value[10], value[11],
-    //                      value[12], value[13], value[14]);
-    // simulate(filename_in.string(), filename_out.string(), wl_to_volume,
-    // wl_to_depth,
-    //          volume_to_wl);
-    std::filesystem::path file_test = "test/data/0DReservoirModel_out.xlsx";
-    FileIO::copyFile(file_test, file_test.parent_path().parent_path() / "temp" /
-                                    file_test.filename());
+    std::string filename_in, filename_out, id;
+    std::optional<double> value[29];
+    parse(argc, argv, filename_in, filename_out, value, id);
+    std::string copyed_file = copyfile_and_write_params(
+        filename_in, "Parameters", id, value[0], value[1], value[2], value[3],
+        value[4], value[5], value[6], value[7], value[8], value[9], value[10],
+        value[11], value[12], value[13], value[14], value[15], value[16],
+        value[17], value[18], value[19], value[20], value[21], value[22],
+        value[23], value[24], value[25], value[26], value[27], value[28]);
+    simulate(copyed_file, filename_out, wl_to_volume, wl_to_depth,
+             volume_to_wl);
     return 0;
 }
