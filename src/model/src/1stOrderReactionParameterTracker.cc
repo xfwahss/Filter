7f72156cf226bb2d7a3c6d837311e11b81a521eb
xfwahss <xfwahss@qq.com>
2024-12-12 09:20:25 +0800

    刺激响应更新策略

--- src/model/src/1stOrderReactionParameterTracker.cc
+++ src/model/src/1stOrderReactionParameterTracker.cc
@@ -1,40 +1,38 @@
 #include <EnsembleKalmanFilter.h>
+#include <algorithm>
+#include <args.hxx>
 #include <config.h>
 #include <string>
 
 // 负责粒子更新步, 将状态向量分配给机理模型
 class Model : public EnsembleModel {
+  private:
+    double lnsqrt(double &&x) { return std::log(x + std::sqrt(1 + x * x)); }
+
   public:
-    static double tau;
+    static double alpha;
+    static double amplitude;
+    double relative_error = 0;
     Eigen::VectorXd predict(const double &dt, const Eigen::VectorXd &status) {
         Eigen::VectorXd s(status.size());
-        s(0)                 = status(0) * std::exp(-status(1) * dt / 10);
-        // Eigen::VectorXd k = const_2derivate_predict(status.segment(1, 3),
-        // dt); s(1) = k(0); s(2) = k(1); s(3) = k(2);
-
-        // 类似生态系统的模拟
-        Eigen::VectorXd diff = get_diff_obs_prior();
-        s(1)                 = status(1) + status(2) * dt;
-        s(2)                 = status(2) * std::exp(-std::abs(status(2)) / tau * dt) + status(3);
-        if (diff(0) > tau || diff(0) < (-tau)) {
-            double re = diff(0);
-            double amplitude;
-            amplitude = - tau * std::log(re + std::sqrt(1 + re * re)) / dt;
-            // sigmoid 函数变体
-            // amplitude = -tau * 2 * (1 / (1 + std::exp(-re)) - 0.5);
-            s(3)      = amplitude;
+        relative_error = get_diff_obs_prior()[0]; // 读取相对误差
+        if (std::abs(relative_error) > alpha) {
+            s(3)        = -lnsqrt(std::move(relative_error)) / dt;
+            double prev = status(3) * (1 - amplitude);
+            s(2)        = status(2) * amplitude * std::exp(-amplitude * dt) + (s(3) - prev) * amplitude;
         } else {
             s(3) = 0;
+            s(2) = status(2) * amplitude * std::exp(-amplitude * dt);
         }
-        if (s(3) * s(2) < 0) {
-            s(2) = s(3);
-        }
+        s(1) = std::max(status(1) + s(2) * dt, 0.0);
+        s(0) = status(0) * std::exp(-s(1) * dt);
         return s;
     }
 };
-double Model::tau = 0.2;
+double Model::alpha     = 0.2;
+double Model::amplitude = 0.10;
 
-int main() {
+int main(int argc, char **argv) {
     if (std::string(DEBUG_LEVEL) == std::string("DEBUG")) {
         // logger::get("Ensemble RE")->set_level(spdlog::level::debug);
         // logger::get("umath")->set_level(spdlog::level::debug);
@@ -42,15 +40,39 @@ int main() {
         // Logger::set_level("const std::string &name", "debug");
         // std::cout << Logger::logger_set[0] << std::endl;
     }
+    args::ArgumentParser parser("1stOrderReactionParameterTracker", "END");
+    args::ValueFlag<std::string> fi(parser, "filein", "file input", {'i'});
+    args::ValueFlag<std::string> fo(parser, "fileout", "file output", {'o'});
+    try {
+        parser.ParseCLI(argc, argv);
+    } catch (args::Help) {
+        std::cout << parser;
+    } catch (args::ParseError e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+    } catch (args::ValidationError e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+    }
+    std::string filename_in, filename_out;
+    if (fi) {
+        filename_in = args::get(fi);
+    }
+    if (fo) {
+        filename_out = args::get(fo);
+    }
 
-    FilterIO modelio("../test/data/Degradation_in.xlsx", "../test/output/Degradation_out.xlsx");
+    FilterIO modelio(filename_in, filename_out);
     std::unordered_map<std::string, double> params = modelio.get_params();
     int ensemble_size                              = params["size"];
     double dt                                      = params["dt"];
     int status_dim                                 = params["status_dims"];
     int obs_dim                                    = params["obs_dims"];
     double get_tau                                 = params["tau"];
-    Model::tau                                     = get_tau;
+    double get_amplitude                           = params["amplitude"];
+
+    Model::alpha                                   = get_tau;
+    Model::amplitude                               = get_amplitude;
     EnsembleKalmanFilter<Model> enkal(ensemble_size);
     spdlog::info("The size of ensemble is: {}", ensemble_size);
     spdlog::info("dt is: {}", dt);
