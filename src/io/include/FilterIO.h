ee08c7dabea7e392d38328b5102162dcbe590912
xfwahss <xfwahss@qq.com>
2024-12-16 15:31:42 +0800

    维FilterIO添加读取矩阵的功能

--- src/io/include/FilterIO.h
+++ src/io/include/FilterIO.h
@@ -15,39 +15,35 @@ class FilterIO {
     void init(const std::string &filename_in, const std::string &filename_out);
     int get_index();
     void increment_index();
-    Eigen::VectorXd get_init_X(const std::string &sheet_name = "Init_X",
-                               const int &column = 2, const int &start_row = 1,
-                               const int &element_nums = 0);
+    Eigen::VectorXd get_init_X(const std::string &sheet_name = "Init_X", const int &column = 2,
+                               const int &start_row = 1, const int &element_nums = 0);
 
-    Eigen::MatrixXd get_init_P(const std::string &sheet_name = "Init_P",
-                               const int &start_row          = 1,
-                               const int &start_column       = 1,
-                               const int &nums               = 0);
+    Eigen::MatrixXd get_init_P(const std::string &sheet_name = "Init_P", const int &start_row = 1,
+                               const int &start_column = 1, const int &nums = 0);
 
-    Eigen::MatrixXd get_H(const std::string &sheet_name = "H",
-                          const int &start_row = 1, const int &start_column = 1,
+    Eigen::MatrixXd get_matrix(const std::string &sheet_name, const int &start_row = 1, const int &start_col = 1,
+                               const int &rows = 0, const int &cols = 0);
+
+    Eigen::MatrixXd get_H(const std::string &sheet_name = "H", const int &start_row = 1, const int &start_column = 1,
                           const int &rows = 0, const int &columns = 0);
 
-    Eigen::VectorXd get_Q(const std::string &sheet_name = "Q",
-                          const int &column = 2, const int &start_row = 1,
+    Eigen::VectorXd get_Q(const std::string &sheet_name = "Q", const int &column = 2, const int &start_row = 1,
                           const int &element_nums = 0);
 
-    std::unordered_map<std::string, double>
-    get_params(const std::string &sheet_name = "Params",
-               const int &index_column = 1, const int &value_column = 2,
-               const int &start_row = 1);
+    std::unordered_map<std::string, double> get_params(const std::string &sheet_name = "Params",
+                                                       const int &index_column = 1, const int &value_column = 2,
+                                                       const int &start_row = 1);
 
-    void write_x(const Eigen::VectorXd &x, const std::string &sheet_name = "X",
-                 const int &start_row = 2, const int &start_column = 2);
+    void write_x(const Eigen::VectorXd &x, const std::string &sheet_name = "X", const int &start_row = 2,
+                 const int &start_column = 2);
 
-    void write_P(const Eigen::MatrixXd &P, const std::string &sheet_name = "P",
-                 const int &start_row = 2, const int &start_column = 2);
+    void write_P(const Eigen::MatrixXd &P, const std::string &sheet_name = "P", const int &start_row = 2,
+                 const int &start_column = 2);
 
     // 读取的数据存入z和R,
     // index表明第index次观测
     // 传入file参数提供ExcelIO的文件读写方法
-    virtual void read_one(ExcelIO &file, Eigen::VectorXd &z, Eigen::MatrixXd &R,
-                          const int &index);
+    virtual void read_one(ExcelIO &file, Eigen::VectorXd &z, Eigen::MatrixXd &R, const int &index);
     void read(Eigen::VectorXd &z, Eigen::MatrixXd &R);
     void write_headers();
     // 记录同化的数据结果
--- src/io/src/FilterIO.cc
+++ src/io/src/FilterIO.cc
@@ -3,8 +3,7 @@
 
 // 负责状态观测步，用于将观测数据由文件读入转化为均值和方差
 // 初始状态读取 和 相关向量的给定
-FilterIO::FilterIO(const std::string &filename_in,
-                   const std::string &filename_out)
+FilterIO::FilterIO(const std::string &filename_in, const std::string &filename_out)
     : file_in(filename_in, "r"), file_out(filename_out, "w"), index(0) {
     total_nums = get_params("Params", 1, 2, 1)["measurement_nums"];
 };
@@ -12,58 +11,48 @@ FilterIO::~FilterIO(){};
 void FilterIO::increment_index() { ++index; }
 int FilterIO::get_index() { return index; }
 
-Eigen::VectorXd FilterIO::get_init_X(const std::string &sheet_name,
-                                     const int &column, const int &start_row,
+Eigen::VectorXd FilterIO::get_init_X(const std::string &sheet_name, const int &column, const int &start_row,
                                      const int &element_nums) {
     // 第一列存储变量的介绍，第二列存储数据
-    Eigen::VectorXd X =
-        file_in.read_column(sheet_name, column, start_row, element_nums);
+    Eigen::VectorXd X = file_in.read_column(sheet_name, column, start_row, element_nums);
     return X;
 }
-Eigen::MatrixXd FilterIO::get_init_P(const std::string &sheet_name,
-                                     const int &start_row,
-                                     const int &start_column, const int &nums) {
-    int dims = nums == 0 ? file_in.get_rows(sheet_name) : nums;
+Eigen::MatrixXd FilterIO::get_init_P(const std::string &sheet_name, const int &start_row, const int &start_column,
+                                     const int &nums) {
+    int dims          = nums == 0 ? file_in.get_rows(sheet_name) : nums;
     // 从最左上角存储数据
-    Eigen::MatrixXd P =
-        file_in.read_block(sheet_name, start_row, start_column, dims, dims);
+    Eigen::MatrixXd P = file_in.read_block(sheet_name, start_row, start_column, dims, dims);
     return P;
 }
-Eigen::MatrixXd FilterIO::get_H(const std::string &sheet_name,
-                                const int &start_row, const int &start_column,
+Eigen::MatrixXd FilterIO::get_matrix(const std::string &sheet_name, const int &start_row, const int &start_col,
+                                     const int &rows, const int &cols) {
+    return file_in.read_block(sheet_name, start_row, start_col, rows, cols);
+}
+Eigen::MatrixXd FilterIO::get_H(const std::string &sheet_name, const int &start_row, const int &start_column,
                                 const int &rows, const int &columns) {
-    int column_nums = columns == 0 ? file_in.get_columns(sheet_name) : columns;
-    int row_nums    = rows == 0 ? file_in.get_rows(sheet_name) : rows;
-    Eigen::MatrixXd H = file_in.read_block(sheet_name, start_row, start_column,
-                                           row_nums, column_nums);
+    int column_nums   = columns == 0 ? file_in.get_columns(sheet_name) : columns;
+    int row_nums      = rows == 0 ? file_in.get_rows(sheet_name) : rows;
+    Eigen::MatrixXd H = file_in.read_block(sheet_name, start_row, start_column, row_nums, column_nums);
     return H;
 }
-Eigen::VectorXd FilterIO::get_Q(const std::string &sheet_name,
-                                const int &column, const int &start_row,
+Eigen::VectorXd FilterIO::get_Q(const std::string &sheet_name, const int &column, const int &start_row,
                                 const int &element_nums) {
     // 第一列存储变量的介绍，第二列存储数据, 给单个预测粒子添加方差
-    Eigen::VectorXd Q =
-        file_in.read_column(sheet_name, column, start_row, element_nums);
+    Eigen::VectorXd Q = file_in.read_column(sheet_name, column, start_row, element_nums);
     return Q;
 }
 
-std::unordered_map<std::string, double>
-FilterIO::get_params(const std::string &sheet_name, const int &index_column,
-                     const int &value_column, const int &start_row) {
+std::unordered_map<std::string, double> FilterIO::get_params(const std::string &sheet_name, const int &index_column,
+                                                             const int &value_column, const int &start_row) {
     std::unordered_map<std::string, double> params;
-    if (file_in.read_cell_string(sheet_name, start_row, index_column) !=
-        "param_nums") {
-        throw std::invalid_argument(
-            "Please set the number of parameters to be "
-            "read as the first parameter, format as 'param_nums 1'");
+    if (file_in.read_cell_string(sheet_name, start_row, index_column) != "param_nums") {
+        throw std::invalid_argument("Please set the number of parameters to be "
+                                    "read as the first parameter, format as 'param_nums 1'");
     } else {
-        int element_nums =
-            file_in.read_column(sheet_name, value_column, start_row, 1)(0);
+        int element_nums = file_in.read_column(sheet_name, value_column, start_row, 1)(0);
         for (int i = 1; i < element_nums + 1; ++i) {
-            std::string index = file_in.read_cell_string(
-                sheet_name, i + start_row, index_column);
-            params[index] = file_in.read_column(sheet_name, value_column,
-                                                start_row + i, 1)(0);
+            std::string index = file_in.read_cell_string(sheet_name, i + start_row, index_column);
+            params[index]     = file_in.read_column(sheet_name, value_column, start_row + i, 1)(0);
         }
     }
     return params;
@@ -81,12 +70,12 @@ void FilterIO::write_headers() {
     file_out.remove_sheet("Sheet1");
 }
 // 记录同化的数据结果
-void FilterIO::write_x(const Eigen::VectorXd &x, const std::string &sheet_name,
-                       const int &start_row, const int &start_column) {
+void FilterIO::write_x(const Eigen::VectorXd &x, const std::string &sheet_name, const int &start_row,
+                       const int &start_column) {
     file_out.write_row(x, sheet_name, index + start_row, start_column);
 }
-void FilterIO::write_P(const Eigen::MatrixXd &P, const std::string &sheet_name,
-                       const int &start_row, const int &start_column) {
+void FilterIO::write_P(const Eigen::MatrixXd &P, const std::string &sheet_name, const int &start_row,
+                       const int &start_column) {
     int nums = P.cols();
     Eigen::VectorXd values(nums);
     for (int i = 0; i < nums; ++i) {
@@ -101,7 +90,7 @@ void FilterIO::read(Eigen::VectorXd &z, Eigen::MatrixXd &R) {
 
         logger::get()->info("Processing measurement of index: {}", index + 1);
         // std::cout << "Processing measurement value of index: " << index + 1
-                //   << std::endl;
+        //   << std::endl;
         read_one(file_in, z, R, index);
         increment_index();
     } else {
@@ -110,12 +99,10 @@ void FilterIO::read(Eigen::VectorXd &z, Eigen::MatrixXd &R) {
     }
 }
 
-void FilterIO::read_one(ExcelIO &file, Eigen::VectorXd &z, Eigen::MatrixXd &R,
-                        const int &index) {
+void FilterIO::read_one(ExcelIO &file, Eigen::VectorXd &z, Eigen::MatrixXd &R, const int &index) {
     z                            = file.read_row("Obs", index + 2, 2);
     Eigen::VectorXd vector_rread = file.read_row("R", index + 2, 2);
-    Eigen::MatrixXd r_read =
-        Eigen::MatrixXd::Zero(vector_rread.size(), vector_rread.size());
+    Eigen::MatrixXd r_read       = Eigen::MatrixXd::Zero(vector_rread.size(), vector_rread.size());
     for (int i = 0; i < vector_rread.size(); ++i) {
         r_read(i, i) = vector_rread(i);
     }
